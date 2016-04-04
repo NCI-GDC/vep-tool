860b122499f683000f3a7b356fde669b38e56e62
Kyle Hernandez <khernandez@bsd.uchicago.edu>
2016-04-04 21:08:51 +0000

    feat(vep-plugins): addin perl plugin files

---
+++ vep-plugins/GDC_evidence.pm
@@ -0,0 +1,146 @@
+=head1 LICENSE
+
+Copyright [2016-2017] University of Chicago
+
+=head1 CONTACT
+
+  Kyle Hernandez <khernandez@bsd.uchicago.edu>
+
+=head1 NAME
+
+ GDC_validation
+
+=head1 SYNOPSIS
+
+ perl variant_effect_predictor.pl -i variations.vcf --plugin GDC_evidence,<variation.vcf.gz>
+
+=head1 DESCRIPTION
+
+ A VEP plugin that adds in the ENSEMBL evidence annotations for dbSNPs. The variation VCF
+ from ENSEMBL is required and should be tabix indexed. TABIX needs to be in your path. 
+
+=cut
+
+package GDC_evidence;
+
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::Variation::Utils::VEP qw(parse_line);
+use Bio::EnsEMBL::Variation::Utils::BaseVepPlugin;
+
+use base qw(Bio::EnsEMBL::Variation::Utils::BaseVepPlugin);
+
+sub new {
+    my $class = shift;
+
+    my $self = $class->SUPER::new(@_);
+
+    # Test tabix
+    die "ERROR: tabix does not seem to be in your path\n" unless `which tabix 2>&1` =~ /tabix$/;
+
+    # get files
+    my $var_vcf = $self->params->[0];
+ 
+    # check files exist
+    die "ERROR: Tabix index file $var_vcf\.tbi not found - perhaps you need to create it first?\n" 
+        unless -e $var_vcf.'.tbi'; 
+
+    # Set
+    $self->{var_vcf}   = $var_vcf;
+    return $self;
+}
+
+sub feature_types {
+  return ['Feature', 'Intergenic'];
+}
+
+sub get_header_info {
+  my $self = shift;
+  return {
+    EVIDENCE => 'Evidence_Status'
+  } 
+}
+
+sub run {
+    my ($self, $tva, $other) = @_;
+
+    # Data hash
+    my %data;
+
+    # Variation feature
+    my $vf = $tva->variation_feature;
+
+    # Existing variation 
+    my $existing = $other->{Existing_variation};
+
+    # If there is an rsID continue
+    if ( $existing =~ /rs/ ) {
+
+      # Get start and stop positions appropriately offset
+      my ($s, $e) = ($vf->{start} - 1, $vf->{end} + 1);
+
+      # Make pos string for tabix
+      my $pos_string = sprintf("%s:%i-%i", $vf->{chr}, $s, $e);
+
+      # Pull out the dbSNP id from the existing variation
+      my @dbsnpids = grep { $_ =~ /^rs/ } split ",", $existing;
+
+      # Only handle where there is a single rsid
+      if( scalar(@dbsnpids) == 1 ) {
+          # Pull out dbSNP id
+          my $dbsnpid = pop(@dbsnpids);
+
+          # Parse validation
+          my $validation  = $self->getValidation($dbsnpid, $pos_string, $vf); 
+
+          # If you found validation info set
+          if( $validation ) {
+              $data{EVIDENCE} = $validation; 
+          }
+      } 
+      elsif( scalar(@dbsnpids) > 1 ) {
+          warn "Multiple dbsnpids not handled!! $pos_string\n";
+      }
+    }
+
+    # Return data 
+    return \%data; 
+}
+
+sub getValidation {
+    my ($self, $dbsnpid, $pos_string, $vf) = @_;
+
+    my $validation;
+
+    open TABIX, sprintf("tabix -f %s %s |", $self->{var_vcf}, $pos_string);
+
+    while(<TABIX>) {
+        chomp;
+        s/\r$//g;        
+
+        # parse VCF line into a VariationFeature object
+        my ($vcf_vf) = @{parse_line({format => 'vcf'}, $_)};
+
+        # check parsed OK
+        next unless $vcf_vf && $vcf_vf->isa('Bio::EnsEMBL::Variation::VariationFeature');
+
+        # compare coords
+        next unless $vcf_vf->{start} == $vf->{start} && $vcf_vf->{end} == $vf->{end};
+
+        # compare ids
+        next unless $vcf_vf->name eq $dbsnpid;
+
+        # Parse info 
+        my @cols    = split /\t/, $_;
+
+        # Parse out the flags from the INFO col for evidence 
+        my @vals    = grep{ $_ =~ m/^E_/g } split(/;/, $cols[7]);
+        $validation = join(",", @vals);
+        last; 
+    }
+    close TABIX;
+    return $validation; 
+}
+
+1;
---
+++ vep-plugins/utils/make_ensembl_entrez_json.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+'''
+Accessory script for making the JSON object to use in the GDC entrez plugin.
+'''
+from __future__ import print_function
+import json
+import sys
+import gzip
+
+__author__='Kyle Hernandez'
+
+def load_gencode(fil):
+    '''Loads the gencode data into a JSON object'''
+    dic  = {}
+
+    try:
+        fh = open(fil)
+    except:
+        print('ERROR!!', 'Unable to open input file', fil, file=sys.stderr) 
+    else:
+        fh.close()
+
+    # Open
+    if fil.endswith('gz'): fh = gzip.open(fil, 'rb')
+    else: fh = gzip.open(fil, 'rU')
+
+    # Process
+    for line in fh: 
+        tid, entrez = line.rstrip('\r\n').split('\t')
+        # Need the ENS Transcript ID not the version
+        tid = tid.split('.')[0]
+        if entrez:
+            if tid not in dic: dic[tid] = []
+            dic[tid].append(int(entrez)) 
+
+    fh.close()
+
+    return dic
+
+def load_ncbi(fil):
+    '''Loads the ncbi data into a JSON object'''
+    dic  = {}
+
+    try:
+        fh = open(fil)
+    except:
+        print('ERROR!!', 'Unable to open input file', fil, file=sys.stderr) 
+    else:
+        fh.close()
+
+    # Open
+    if fil.endswith('gz'): fh = gzip.open(fil, 'rb')
+    else: fh = gzip.open(fil, 'rU')
+
+    # Process
+    head = []
+    for line in fh: 
+        # comment lines start with '#'
+        if line.startswith('#'): continue
+        else:
+            # This assumes that the first column is tax_id
+            # second column is GeneID and third column is Symbol
+            # Also, geneID and symbol should always be defined
+            cols   = line.rstrip('\r\n').split('\t')
+            entrez = int(cols[1])
+            symbol = cols[2]
+            if symbol not in dic: dic[symbol] = []
+            dic[symbol].append(entrez)
+
+    fh.close()
+
+    return dic
+
+def print_usage():
+    '''Prints usage to stderr'''
+    print('''
+-------------------------------------------------------------------------------
+Converts the Gencode Entrez Gene IDs file:
+<ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_22/gencode.v22.metadata.EntrezGene.gz>
+
+and the NCBI human gene info file:
+<ftp://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO/Mammalia/Homo_sapiens.gene_info.gz>
+
+to a JSON object for use with the GDC_entrez plugin
+-------------------------------------------------------------------------------
+
+    python make_ensembl_entrez_json.py <gencode_entrez_gene_file> <ncbi_gene_info_file> <output_json_file>
+''', file=sys.stderr)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print_usage()
+        sys.exit(1)
+
+    # Positionals
+    gencode_fil  = sys.argv[1]
+    ncbi_fil     = sys.argv[2]
+    ofil         = sys.argv[3]
+
+    # Main container
+    data = {'GENCODE': {}, 'NCBI': {}}
+
+    # Load gencode
+    data['GENCODE'] = load_gencode(gencode_fil)
+
+    # Load ncbi 
+    data['NCBI'] = load_ncbi(ncbi_fil)
+
+    # Write
+    with open(ofil, 'wb') as o:
+        json.dump(data, o) 
