a48872722922c70ee91cc5234d41c0ce1e311524
Kyle Hernandez <khernandez@bsd.uchicago.edu>
2016-04-27 20:12:22 -0500

    feat(md5): adding md5

--- vep-tool/metrics/base_metrics.py
+++ vep-tool/metrics/base_metrics.py
@@ -1,6 +1,9 @@
 '''
-Base class for CWL metrics tool
+Base classes for CWL metrics tool
 '''
+import hashlib
+import gzip
+
 from cdis_pipe_utils import time_util
 
 class CWLMetricsTool(object):
@@ -21,3 +24,34 @@ class CWLMetricsTool(object):
 
     def add_metrics(self):
         pass
+
+class CWLMetricsMd5Tool(object):
+    BLOCKSIZE = 65536
+
+    def __init__(self, time_file, normal_id, tumor_id, input_uuid, output_uuid, case_id, engine, input_file):
+        self.time_file   = time_file
+        self.normal_id   = normal_id
+        self.tumor_id    = tumor_id
+        self.input_uuid  = input_uuid
+        self.output_uuid = output_uuid
+        self.case_id     = case_id
+        self.engine      = engine
+        self.input_file  = input_file
+
+    def get_time_metrics(self):
+        time_str = None
+        with open(self.time_file, 'rb') as fh:
+            time_str = fh.read()
+        return time_util.parse_time(time_str)
+
+    def add_metrics(self):
+        pass
+
+    def get_gz_md5(self):
+        hasher = hashlib.md5()
+        with gzip.open(self.input_file, 'rb') as afile:
+            buf = afile.read(BLOCKSIZE)
+            while len(buf) > 0:
+                hasher.update(buf)
+                buf = afile.read(BLOCKSIZE)
+        return hasher.hexdigest()
--- vep-tool/metrics/vep.py
+++ vep-tool/metrics/vep.py
@@ -1,23 +1,25 @@
 '''
 Metrics table class for VEP 
 '''
-from metrics.mixins import CustomToolTypeMixin
-from metrics.base_metrics import CWLMetricsTool
+from metrics.mixins import CustomToolMd5TypeMixin
+from metrics.base_metrics import CWLMetricsMd5Tool
 
+from utils.hash_md5 import get_gz_md5 
 from cdis_pipe_utils import postgres
 
-class VEPMetricsTable(CustomToolTypeMixin, postgres.Base):
+class VEPMetricsTable(CustomToolMd5TypeMixin, postgres.Base):
 
     __tablename__ = 'vep_metrics'
 
-class VEPMetricsTool(CWLMetricsTool):
-    def __init__(self, time_file, normal_id, tumor_id, input_uuid, output_uuid, case_id, engine):
-        super(VEPMetricsTool,self).__init__(time_file, normal_id, tumor_id, input_uuid, output_uuid, case_id, engine)
+class VEPMetricsTool(CWLMetricsMd5Tool):
+    def __init__(self, time_file, normal_id, tumor_id, input_uuid, output_uuid, case_id, engine, input_file):
+        super(VEPMetricsTool,self).__init__(time_file, normal_id, tumor_id, input_uuid, output_uuid, case_id, engine, input_file)
         self.tool  = 'vep'
         self.files = [normal_id, tumor_id]
 
     def add_metrics(self):
         time_metrics = self.get_time_metrics()
+        md5          = self.get_gz_md5()
         metrics      = VEPMetricsTable(case_id           = self.case_id,
                                        vcf_id            = self.output_uuid,
                                        src_vcf_id        = self.input_uuid,
@@ -27,6 +29,7 @@ class VEPMetricsTool(CWLMetricsTool):
                                        usertime          = time_metrics['user_time'],
                                        elapsed           = time_metrics['wall_clock'],
                                        cpu               = time_metrics['percent_of_cpu'],
-                                       max_resident_time = time_metrics['maximum_resident_set_size'])
+                                       max_resident_time = time_metrics['maximum_resident_set_size'],
+                                       md5               = md5)
         postgres.create_table(self.engine, metrics)
         postgres.add_metrics(self.engine, metrics)
